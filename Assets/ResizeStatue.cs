f937a74d7ce364ce702aab17e947a6255a207bf0
Sougata Dafader <sougatadafader@gmail.com>
2017-11-19 10:27:22 -0500

    2

---
+++ Assets/ResizeStatue.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeStatue : MonoBehaviour {
+
+    public float size;
+    Vector3 temp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void StatueResize(float newSize)
+    {
+                transform.localScale = new Vector3(newSize , newSize , newSize);
+        /*
+        Doesn't Work
+        transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * 2f * UiSlider.current.value);
+        */
+
+        //transform.localScale = transform.localScale * (newSize * Time.deltaTime);
+
+        /*
+        this.transform.localScale += new Vector3(newSize * Time.deltaTime, size * Time.deltaTime, size * Time.deltaTime);
+        size = newSize;
+
+        if (newSize < size)
+        {
+            this.transform.localScale -= new Vector3(newSize * Time.deltaTime, size * Time.deltaTime, size * Time.deltaTime);
+        } 
+        */
+
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+#if UNITY_ANDROID || UNITY_IOS
+
+    float moveSpeed;
+    // Use this for initialization
+    void Start () {
+        moveSpeed = 5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1)
+        {
+            //Store input
+            Touch fing = Input.GetTouch(0);
+            if (fing.phase == TouchPhase.Moved)
+            {  //If the finger has moved since the last frame
+                //Find the amount of finger movement, and apply a rotation to this gameobject based on that amount
+                Vector3 rotation = transform.localRotation.eulerAngles;
+                Vector2 fingMove = fing.deltaPosition;
+                float deltaY = fingMove.x * moveSpeed * Time.deltaTime*2;
+                rotation.y -= deltaY;
+                this.transform.localRotation = Quaternion.Euler(rotation);
+            }
+        }
+    }
+#endif
+}
